c68330cfe6b344cca10fda6a2bf2cf80cb24b3b5
CarlosRivera05 <141857513+CarlosRivera05@users.noreply.github.com>
2024-02-28 13:58:48 -0400

    Create Vector.h

---
+++ Vector.h
@@ -0,0 +1,15 @@
+// Andrés Osinaga
+#pragma once
+class Vector{
+private:
+	int n, vec[10];
+public:
+	Vector(void); //Constructor
+	~Vector(void); //Destructor
+	
+	void set_n(int _n);
+	int get_n();
+	void cargarVector(int vec[], int n); //metodos
+	void mostrarVector(int vec[], int n);
+	void ordenarVector(int vec[], int n);
+};
